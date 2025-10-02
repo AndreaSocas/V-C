bc14756557abbd08521a2fbea2562cddc476e929
Andrea <andrea.socas.exposito@gmail.com>
2025-10-02 02:35:44 +0100

    base

---
+++ resources/views/TextosLegales/cookies.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="TextosLegalesContenedor">
+    <div class="TextosLegalesTarjeta">
+        <h1>{{ __('cookies.Coo_01') }}</h1>
+        <h2>{{-- NOMBRE DE LA WEB --}}</h2>
+        
+        <h3>{{ __('cookies.Coo_02') }}</h3>
+        <p>{{ __('cookies.Coo_03') }}</p>
+        <p>{!! __('cookies.Coo_04') !!}</p>
+        <p>{{ __('cookies.Coo_05') }}</p>
+
+        <h3>{{ __('cookies.Coo_08') }}</h3>
+        <p>{{ __('cookies.Coo_09') }}</p>
+
+        <h3>{{ __('cookies.Coo_10') }}</h3>
+        <p>{{ __('cookies.Coo_11') }}</p>
+
+        <h3>{{ __('cookies.Coo_12') }}</h3>
+        <p>{{ __('cookies.Coo_13') }}</p>
+
+        <h3>{{ __('cookies.Coo_14') }}</h3>
+        <p>{{ __('cookies.Coo_15') }}</p>
+
+        <h3>{{ __('cookies.Coo_16') }}</h3>
+        <p>{{ __('cookies.Coo_17') }}</p>
+
+        <h3>{{ __('cookies.Coo_18') }}</h3>
+        <p>{!! __('cookies.Coo_19') !!}</p>
+
+        <h3>{{ __('cookies.Coo_20') }}</h3>
+        <p>{!! __('cookies.Coo_21') !!}</p>
+
+        <h3>{{ __('cookies.Coo_22') }}</h3>
+        <p>{{ __('cookies.Coo_23') }}</p>
+    </div>
+</div>
+@endsection
---
+++ resources/views/TextosLegales/creditos.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="TextosLegalesContenedor">
+    <div class="TextosLegalesTarjeta">
+        <h1>{{ __('creditos.Cred_01') }}</h1>
+        <h2>{{-- URL --}}</h2>
+        
+        <h3>{{ __('creditos.Cred_02') }}</h3>
+        <ul>
+            <li></li>
+        </ul>
+        <br>
+
+        <h3>{{ __('creditos.Cred_03') }}</h3>
+        <ul>
+            <li>Uicons by <a href="https://www.flaticon.com/uicons">Flaticon</a></li>
+        </ul>
+    </div>
+</div>
+	
+@endsection
